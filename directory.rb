9933f48a73f30752b4e43a49276a82152418eb35
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-09 20:40:59 +0100

    add interactive menu step9

--- directory.rb
+++ directory.rb
@@ -98,11 +98,36 @@ end
    puts "Overall, we have #{students.count} great students"
  end
 end
- #nothing happens until we call the methods
-students = input_students
-input_cohort(students)
-student_information(students)
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+      puts "1. Input the students"
+      puts "2. Show the students"
+      puts "9. Exit" # 9 because we'll be adding more items
+      # 2. read the input and save it to a variable
+      selection = gets.chomp
+      # 3. do what the user has asked
+      case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print_redo(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+      end
+    end
+  end
+ #nothing happens until we call the methods
+# students = input_students
+# input_cohort(students)
+# student_information(students)
+interactive_menu
 print_header
 # group_by_cohort(students)
 print_redo(students)
